ccb32647520976e116906232a634dfaa3dd4cc90
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-02-13 15:07:07 +0200

    add form & usersList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { addUser } from './features/Users'
+import { useState } from 'react'
 
 function App() {
+  const dispatch = useDispatch()
+  const userList = useSelector((state) => state.users.value);
+  const [name, setName] = useState("")
+  const [username, setUsername] = useState("")
+
   return (
-      <div className="App">
-        <header className="App-header">
-            Learn React
-        </header>
+    <div className="App">
+      <div className="addUser">
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange = {(event)=> {
+            setName(event.target.value)
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange = {(event)=> {
+            setUsername(event.target.value)
+          }}
+        />
+        <button
+          onClick = {()=> {
+            dispatch(addUser({id: userList[userList.length - 1].id + 1, name, username}))
+          }}
+        >
+          {" "}
+          Add User
+        </button>
       </div>
-  
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1> {user.name}</h1>
+              <h1> {user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
